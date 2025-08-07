c4ddeb804c325b40e94371717cbe5677603b5b20
jhoglund <jonas@stixy.com>
2025-08-07 12:13:59 +0200

    Update button styles and layout improvements - responsive grid for industry categories is improved and fully functional, build section responsive behavior enhanced

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  def category_card_icon(category_name, options = {})
+  def industry_card_icon(category_name, options = {})
     file_name = convert_to_filename(category_name)
     icon_path = "icons/categories/#{file_name}-icon.svg"
-    image_tag(icon_path, options.merge(class: "category-icon"))
+    image_tag(icon_path, options.merge(class: "industry-icon"))
   end
 
-  def category_card_label(category_name, options = {})
+  def industry_card_label(category_name, options = {})
     file_name = convert_to_filename(category_name)
     label_path = "icons/categories/#{file_name}-label.svg"
-    image_tag(label_path, options.merge(class: "category-label"))
+    image_tag(label_path, options.merge(class: "industry-label"))
   end
 
   private
